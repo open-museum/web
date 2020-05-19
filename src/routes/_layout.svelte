ebefad20c3a11df7432c1ca1196c5d5507557998
Moritz Mähr <maehr@users.noreply.github.com>
2020-05-19 16:03:11 +0200

    Feature/advanced analytics (#24)
    * spa conform matomo analytics with @dexlib/svelte-matomo

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,21 @@
 <script>
     import AudioChat from "./../components/AudioChat.svelte";
+    import Matomo, { matomo } from '@dexlib/svelte-matomo'
+    import { stores } from "@sapper/app";
+    import { onMount } from "svelte";
+
+    const { page } = stores();
+
+    const url = "https://analytics.open-museum.ch";
+    const siteId = 2;
+
+    $: if ($page) matomo.trackPageView();
+
+    onMount(() => {
+        matomo.trackPageView();
+    });
 </script>
 
 <slot />
+<Matomo {url} {siteId} />
 <AudioChat />
